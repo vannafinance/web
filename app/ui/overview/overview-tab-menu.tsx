87460a6d4649f96e02dd83943e139f46f123f27d
Meet Doshi <meetdoshi193@gmail.com>
2024-09-16 12:28:06 +0530

    overview lender section responsiveness done

--- app/ui/overview/overview-tab-menu.tsx
+++ app/ui/overview/overview-tab-menu.tsx
@@ -7,7 +7,7 @@ import BorrowerDashboard from "./borrower-dashboard";
 import Image from "next/image";
 
 const TotalHoldings: React.FC = () => (
-  <div className="bg-white rounded-3xl border border-purpleBG-lighter p-6 mb-7">
+  <div className="bg-white rounded-3xl border border-purpleBG-lighter p-4 lg:p-6 mb-7">
     <div className="flex justify-between items-start mb-10">
       <div>
         <h2 className="text-base font-medium text-neutral-500">
@@ -30,7 +30,7 @@ const TotalHoldings: React.FC = () => (
 );
 
 const Rewards: React.FC = () => (
-  <div className="bg-white rounded-3xl border border-purpleBG-lighter p-10 mt-4">
+  <div className="bg-white rounded-3xl border border-purpleBG-lighter p-4 xl:p-10 mt-4">
     <h2 className="text-xl font-semibold mb-9">Rewards</h2>
     <div className="bg-gray-100 rounded-lg p-2 mb-4">
       <div className="flex justify-between bg-baseComplementary text-base font-medium mb-2">
@@ -39,7 +39,7 @@ const Rewards: React.FC = () => (
         <span>Claim</span>
       </div>
     </div>
-    <div className="px-4 text-baseBlack">
+    <div className="px-2 lg:px-4 text-baseBlack">
       {["WETH", "USDC", "WBTC"].map((pool, index) => (
         <div
           key={index}
@@ -92,12 +92,12 @@ const OverviewTabMenu = () => {
       </div>
 
       <div className="container mx-auto pt-2">
-        <div className="flex flex-col md:flex-row">
-          <div className="w-full md:w-1/3 pr-0 md:pr-4 mb-4 md:mb-0">
+        <div className="flex flex-col lg:flex-row">
+          <div className="w-full lg:w-1/3 pr-0 lg:pr-4 mb-4 lg:mb-0">
             <TotalHoldings />
             <Rewards />
           </div>
-          <div className="w-full md:w-2/3">{renderTabContent()}</div>
+          <div className="w-full lg:w-2/3">{renderTabContent()}</div>
         </div>
       </div>
     </div>
